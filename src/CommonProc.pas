d231d5a91ae779eabf4de099d63c28200dfedfd8
TAV <tereshenkov@mail.ru>
2022-08-03 17:56:15 +0300

    Дополнение к коммиту "заливка вращением фигуры"

--- src/CommonProc.pas
+++ src/CommonProc.pas
@@ -15,7 +15,7 @@ function getY(b:TBlock):Integer ;
 function getZ(b:TBlock):Integer ;
 function isValueBetween(x,a,b:Integer):Boolean ;
 function isBlockEqTexAndType(const b1:TBlock; const b2:TBlock):Boolean ;
-function isZoneReadyForFillRot(const zone:TZone3I):Boolean ;
+function isZoneReadyForRotFill(const zone:TZone3I):Boolean ;
 
 implementation
 uses SysUtils, IOUtils ;
@@ -84,7 +84,7 @@ begin
   Result:=(b1.texcode=b2.texcode)and(b1.bt=b2.bt) ;
 end;
 
-function isZoneReadyForFillRot(const zone:TZone3I):Boolean ;
+function isZoneReadyForRotFill(const zone:TZone3I):Boolean ;
 begin
   Result:=False ;
   if (zone.dX()=1)and(zone.dY()>1)and(zone.dZ()>1) then Exit(True) ;
